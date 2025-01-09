d60ed58f31e0beec61355dced298e09f5114290e
inu <45503605+inu1104@users.noreply.github.com>
2025-01-10 04:08:25 +0800

    Create 2_add_two_numbers.c

---
+++ LeetCode/2_add_two_numbers.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    struct ListNode *next;
+};
+
+typedef struct ListNode node;
+
+node *addTwoNumbers(node *l1, node *l2)
+{
+    int value, carry = 0;
+    node *current, *result = (node *)malloc(sizeof(node));
+    current = result;
+    while (l1 != NULL || l2 != NULL || carry != 0)
+    {
+        value = carry;
+        if (l1 != NULL)
+        {
+            value += l1->val;
+            l1 = l1->next;
+        }
+        if (l2 != NULL)
+        {
+            value += l2->val;
+            l2 = l2->next;
+        }
+        carry = value / 10;
+        value %= 10;
+
+        node *new = (node *)malloc(sizeof(node));
+        new->val = value;
+        new->next = NULL;
+        current->next = new;
+        current = current->next;
+    }
+    current = result;
+    result = result->next;
+    free(current);
+    return result;
+}
