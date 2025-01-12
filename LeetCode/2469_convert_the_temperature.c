f14d4af9025670557907efeddb7882b0f9ff557d
inu <45503605+inu1104@users.noreply.github.com>
2025-01-13 00:58:34 +0800

    Create 2469_convert_the_temperature.c

---
+++ LeetCode/2469_convert_the_temperature.c
@@ -0,0 +1,9 @@
+double *convertTemperature(double celsius, int *returnSize)
+{
+    (*returnSize) = 2;
+    double *result = malloc(2 * sizeof(double));
+    result[0] = celsius + 273.15;
+    result[1] = celsius * 1.80 + 32.00;
+
+    return result;
+}
