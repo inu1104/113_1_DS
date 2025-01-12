61190a76c6646e6e7f2473864ed108cda4b698a2
inu <45503605+inu1104@users.noreply.github.com>
2025-01-13 00:54:06 +0800

    Create 0258_add_digits.c

---
+++ LeetCode/0258_add_digits.c
@@ -0,0 +1,16 @@
+int addDigits(int num)
+{
+    int temp, result = num;
+
+    while (result >= 10)
+    {
+        temp = 0;
+        while (result > 0)
+        {
+            temp += result % 10;
+            result /= 10;
+        }
+        result = temp;
+    }
+    return result;
+}
